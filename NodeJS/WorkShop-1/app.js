2e08c1ef41d23f6215c6acb79cbce4892981f46c
MohdUmar07 <umohd9554@gmail.com>
2023-08-27 12:26:16 +0530

    Learned and practised get() & post() by using express.json()

--- NodeJS/WorkShop-1/app.js
+++ NodeJS/WorkShop-1/app.js
@@ -2,4 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.send("Hello everyone");
+});
+
+app.post('/add', (req,res)=>{
+    let n1 = 3;
+    let n2 = 4;
+    let sum = n1+n2;
+       
+    // res.send("Sum = " + sum);
+    res.send(`Sum = ${sum}`);
+});
+
 app.listen(3000);
