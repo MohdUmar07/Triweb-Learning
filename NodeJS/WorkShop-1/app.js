c087397034e1ea69975daad6916255d259e27249
MohdUmar07 <umohd9554@gmail.com>
2023-08-27 12:47:23 +0530

    Created a calculator using get() & post() and connected to localhost server on Postman

--- NodeJS/WorkShop-1/app.js
+++ NodeJS/WorkShop-1/app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.get('/',(req, res)=>{
-    res.send("Hello everyone");
+    res.send("I am response");
 });
 
-app.post('/add', (req,res)=>{
+app.get('/add', (req,res)=>{
     let n1 = 3;
     let n2 = 4;
     let sum = n1+n2;
@@ -17,4 +17,43 @@ app.post('/add', (req,res)=>{
     res.send(`Sum = ${sum}`);
 });
 
+
+
+app.post('/add',(req, res)=>{
+
+    let n1 = req.body.num1;
+    let n2 = req.body.num2;
+    let sum = n1+n2;
+    res.send(`sum = ${sum}`);
+});
+
+
+app.post('/diffrence',(req, res)=>{
+
+    let n1 = req.body.num1;
+    let n2 = req.body.num2;
+    let diff = n1-n2;
+    res.send(`diffrence = ${diff}`);
+});
+
+
+app.post('/multiply',(req, res)=>{
+
+    let n1 = req.body.num1;
+    let n2 = req.body.num2;
+    let multiply = n1*n2;
+    res.send(`Product = ${multiply}`);
+});
+
+
+app.post('/division',(req, res)=>{
+
+    let n1 = req.body.num1;
+    let n2 = req.body.num2;
+    let quotient = n1/n2;
+    res.send(`Quotient = ${quotient}`);
+});
+
+
 app.listen(3000);
+
