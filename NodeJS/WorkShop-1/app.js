7f8daa1e21a1acb32e361f5a5395f052b5c9aa2b
MohdUmar07 <umohd9554@gmail.com>
2023-08-27 13:43:30 +0530

    Prectised REST API architechture using express routers and controllers

--- NodeJS/WorkShop-1/app.js
+++ NodeJS/WorkShop-1/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const calculatorRouter = require('./routers/calculator');
 
 const app = express();
 
@@ -8,51 +9,18 @@ app.get('/',(req, res)=>{
     res.send("I am response");
 });
 
-app.get('/add', (req,res)=>{
-    let n1 = 3;
-    let n2 = 4;
-    let sum = n1+n2;
+// app.get('/add', (req,res)=>{
+//     let n1 = 3;
+//     let n2 = 4;
+//     let sum = n1+n2;
        
-    // res.send("Sum = " + sum);
-    res.send(`Sum = ${sum}`);
-});
+//     // res.send("Sum = " + sum);
+//     res.send(`Sum = ${sum}`);
+// });
 
 
+app.use('/calculator',calculatorRouter)
 
-app.post('/add',(req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let sum = n1+n2;
-    res.send(`sum = ${sum}`);
-});
-
-
-app.post('/diffrence',(req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let diff = n1-n2;
-    res.send(`diffrence = ${diff}`);
-});
-
-
-app.post('/multiply',(req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let multiply = n1*n2;
-    res.send(`Product = ${multiply}`);
-});
-
-
-app.post('/division',(req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let quotient = n1/n2;
-    res.send(`Quotient = ${quotient}`);
-});
 
 
 app.listen(3000);
