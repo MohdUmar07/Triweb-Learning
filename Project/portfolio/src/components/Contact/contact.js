be6b691193f60b0aece7f70d44d580de81cce55a
MohdUmar07 <umohd9554@gmail.com>
2023-10-20 11:47:06 +0530

    Added EmailJs functionality in contact me section of portfolio

--- Project/portfolio/src/components/Contact/contact.js
+++ Project/portfolio/src/components/Contact/contact.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import { React, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import './contact.css';
 
 import facebook from '../../assets/facebook.png';
@@ -8,14 +10,29 @@ import twitter from '../../assets/twitter.png';
 
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_u6ivzki', 'template_tpzchgh', form.current, 'LweSJTgvb0x0TrT5r')
+          .then((result) => {
+              console.log(result.text);
+              alert("Email Sent!");
+              e.target.reset();
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
   return (
    <section id='contactsPage'>
        <div id='contact'>
             <h1 className='contactPageTitle'>Contact Me</h1>
             <span className='contactDesc'> Please fill the form for any quires and support </span>
-            <form className='cotactForm'>
-                <input type='text' className='name' placeholder='Your Name' />
-                <input type='email' className='email' placeholder='Your Email' />
+            <form ref={form} onSubmit={sendEmail} className='cotactForm'>
+                <input type='text' className='name' placeholder='Your Name' name='from_name' />
+                <input type='email' className='email' placeholder='Your Email' name='from_email' />
                 <textarea className='msg' name='message' rows='5' placeholder='Your Message' />
                 <button type='submit' value='Send' className='submitBtn'>Submit</button>
                 <div className='links'>
